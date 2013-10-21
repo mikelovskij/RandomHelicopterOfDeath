9cfb7c183e9ec4620539d9b73f3904c398577e3e
Mikelovskij <snao20@hotmail.com>
2013-10-21 21:08:26 +0200

    commesso

---
+++ sincronizedRAW/Parametri.h
@@ -0,0 +1,27 @@
+/**************************************************************GENERALI****************************************************************************************/
+#define PERIOD 10//periodo (in millisecondi) del loop principale. per ora il minimo effettivo è 6-7 ms ma aggiungendo cose diventerà facilmente di più.
+
+/**************************************************************FILTERING****************************************************************************************/
+#define FILTERING 4  //numero di elementi da includere in ogni media fatta dall'arduino
+#define DLP 6  //frequenza di taglio del digital low pass filter interno all'mpu secondo la seguente tabella
+/*
+ *          |   ACCELEROMETER    |           GYROSCOPE
+ * DLP      | Bandwidth | Delay  | Bandwidth | Delay  | Sample Rate
+ * ---------+-----------+--------+-----------+--------+-------------
+ * 0        | 260Hz     | 0ms    | 256Hz     | 0.98ms | 8kHz
+ * 1        | 184Hz     | 2.0ms  | 188Hz     | 1.9ms  | 1kHz
+ * 2        | 94Hz      | 3.0ms  | 98Hz      | 2.8ms  | 1kHz
+ * 3        | 44Hz      | 4.9ms  | 42Hz      | 4.8ms  | 1kHz
+ * 4        | 21Hz      | 8.5ms  | 20Hz      | 8.3ms  | 1kHz
+ * 5        | 10Hz      | 13.8ms | 10Hz      | 13.4ms | 1kHz
+ * 6        | 5Hz       | 19.0ms | 5Hz       | 18.6ms | 1kHz
+ * 7        |   -- Reserved --   |   -- Reserved --   | Reserved
+*/
+
+/**********************************************************************DEBUG MODES E LEDS DI DEBUG**************************************************************/
+//#define DEBUG //modalità di debug inclusa nelle leibrerie dell'mpu.Se attivata racconterà sulla seriale step per step tutte le funzioni dell'mpu... non attivare a meno che non si vogliano un sacco di chiacchere in seriale.
+
+#define DEBUG_GYRODATA //questo define abilita invece la trasmissione dei dati del gyro filtrati e di alcune info sull'inizializzazione dell'mpu compatibile con labview
+
+//#define DEBUG_TIMING //stampa via seriale il tempo fra un ciclo e l'altro. rallenta le trasmissioni del debug gyrodata ovviam se sono attivati assieme.
+#define LED_PIN 13
---
+++ sincronizedRAW/sincronizedRAW.ino
@@ -0,0 +1,73 @@
+/* ============================================
+I2Cdev device library code is placed under the MIT license
+Copyright (c) 2011 Jeff Rowberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+
+#include "Parametri.h"
+#include "Wire.h"
+#include "Arduino.h"
+#include <VirtualWire.h>
+#include "Ricevitore_lib.h"
+#include "Debug.h" 
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+//#include "MPU6050.h"  //inutile se c'è già incluso "MPU6050_6Axis_MotionApps20.h"
+#include "prendidati.h"
+
+
+
+
+
+//////////////////////////////////////////////////////////////////DATA////////////////////////////////////////////////////////////////////////////////////////
+long inputdata[6]; //misure fatte dalla libreria che prende i dati dall'arduino e filtra e li sputa. le 6 componenti corrispondono ad ax,ay,az,gx,gy,gz
+
+
+//////////////////////////////////////////////////////////////////SINCRONIZZAZIONE DEL LOOP////////////////////////////////////////////////////////////////////
+unsigned long prev_loop = 0; //buffer che contiene l'ultimo valore di millis usato dal ciclo di sincronizzazione
+unsigned long actual_loop; //valore attuale confrontato con prev (volendo si può rimuovere ma servirebbe richiamare la funzione millis() una volta in più.
+
+
+//////////////////////////////////////////////////////////////ALTRE VARIABILI//////////////////////////////////////////////////////////////////////////////////
+bool blinkState = false;
+
+
+///////////////////////////////////////////////////////////SETUP///////////////////////////////////////////////////////////////////////////////////////////////
+void setup() {
+	DEBUG_BEGIN(115200);//inizializza la seriale. più è veloce più ciuccia risorse credo, ma in compenso perde meno tempo a trasmettere
+	setupmpu();
+	setup_myreciver(5,2);
+	pinMode(LED_PIN, OUTPUT);
+	delay(500);
+}
+
+void loop() {
+	actual_loop = millis();
+	if(actual_loop-prev_loop >=PERIOD){
+		TIMING_PRINTLN(actual_loop-prev_loop); 
+		prev_loop=actual_loop;    
+		prendidati(inputdata);
+		refresh_recived_commands();   
+		blinkState = !blinkState;
+		digitalWrite(LED_PIN, blinkState);
+	}
+	
+}
