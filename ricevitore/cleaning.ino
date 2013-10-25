64a7f51f08c646f00875a1e152e5c28f54434abe
Mikelovskij <snao20@hotmail.com>
2013-10-25 20:00:20 +0200

    bleh

--- ricevitore/cleaning.ino
+++ ricevitore/cleaning.ino
@@ -1,20 +1,3 @@
-// I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050 class using DMP (MotionApps v2.0)
-// 6/21/2012 by Jeff Rowberg <jeff@rowberg.net>
-// Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
-//
-// Changelog:
-//     2012-06-21 - added note about Arduino 1.0.1 + Leonardo compatibility error
-//     2012-06-20 - improved FIFO overflow handling and simplified read process
-//     2012-06-19 - completely rearranged DMP initialization code and simplification
-//     2012-06-13 - pull gyro and accel data from FIFO packet instead of reading directly
-//     2012-06-09 - fix broken FIFO read sequence and change interrupt detection to RISING
-//     2012-06-05 - add gravity-compensated initial reference frame acceleration output
-//                - add 3D math helper file to DMP6 example sketch
-//                - add Euler output and Yaw/Pitch/Roll output formats
-//     2012-06-04 - remove accel offset clearing for better results (thanks Sungon Lee)
-//     2012-06-01 - fixed gyro sensitivity to be 2000 deg/sec instead of 250
-//     2012-05-30 - basic DMP initialization working
-
 /* ============================================
 I2Cdev device library code is placed under the MIT license
 Copyright (c) 2012 Jeff Rowberg
@@ -166,13 +149,6 @@ void setup() {
     // the baud timing being too misaligned with processor ticks. You must use
     // 38400 or slower in these cases, or use some kind of external separate
     // crystal solution for the UART timer.
-    
-    pinMode(DEBUG_1, OUTPUT);
-    pinMode(DEBUG_2, OUTPUT);
-    pinMode(DEBUG_3, OUTPUT);
-    pinMode(DEBUG_4, OUTPUT);
-    pinMode(DEBUG_5, OUTPUT);
-    pinMode(DEBUG_6, OUTPUT);
     
     // initialize device
     Serial.println(F("Initializing I2C devices..."));
